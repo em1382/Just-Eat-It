5f332c41a480849443bcedafcfdab9895a1651ee
nik <ns1302@messiah.edu>
2018-12-12 19:30:14 -0500

    test 1 fix

--- components/__tests__/StyledText-test.js
+++ components/__tests__/StyledText-test.js
@@ -4,18 +4,7 @@ import { MonoText } from '../StyledText';
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  const tree = renderer.create(<Text
-      style={
-        Array [
-          undefined,
-          Object {
-            "fontFamily": "space-mono",
-          },
-        ]
-      }
-    >
-      Snapshot test!
-    </Text>).toJSON();
+  const tree = renderer.create(<MonoText>Snapshot test!</MonoText>).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
